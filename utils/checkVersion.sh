0d6ce23534ab131d312cbdeb26f90719cb9e705f
m4n3dw0lf <m4n3dw0lf@gmail.com>
2023-12-22 17:02:05 -0300

    Fixed #6

---
+++ utils/checkVersion.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GIT_TAG=$(git tag)
+REPO_VERSION=$(git tag | tr -d "v" | grep "${VERSION}")
+
+if [[ -z "${VERSION}" ]];then
+    echo "Abort Version is null !!!!!!"
+    echo "Add version in setup.yml"
+    exit 1
+fi
+
+if [[ ! -z "${REPO_VERSION}" ]] && [[ ! -z "${GIT_TAG}" ]] && [[ "${VERSION}" == "${REPO_VERSION}"  ]];then
+    echo "Abort Version: ${VERSION} exist !!!!!!"
+    echo "Change version in setup.yml"
+    exit 1
+fi
